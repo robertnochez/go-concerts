25576a5d50f653d5df02902f1abc92444260c1f3
Christy Huang <hchristy1234@gmail.com>
2024-09-14 19:44:24 -0400

    fix erroring section hopefully

--- app/page.tsx
+++ app/page.tsx
@@ -96,7 +96,7 @@ function GetStartedButton() {
       type="submit"
       className="flex items-center justify-center px-8 py-4 mb-2 me-2 overflow-hidden text-xl font-medium text-gray-100 rounded-xl group bg-gradient-to-br from-purple-600 to-orange-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:focus:ring-blue-800"
     >
-      <span className="flex items-center gap-1">
+      {/* <span className="flex items-center gap-1">
         {pending ? (
           <span className=" px-16">
             <Loader className="w-5 h-5" />
@@ -107,7 +107,7 @@ function GetStartedButton() {
             <StepForward />
           </>
         )}
-      </span>
+      </span> */}
     </button>
   );
 }
