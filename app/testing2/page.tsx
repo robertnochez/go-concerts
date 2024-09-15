460dabd1a421a30f203a0d1aabbf014967faff29
Christy Huang <hchristy1234@gmail.com>
2024-09-15 10:38:46 -0400

    minor styling changes

--- app/testing2/page.tsx
+++ app/testing2/page.tsx
@@ -185,7 +185,7 @@ function SignInButton() {
     <Link href={"/signIn"}>
       <button
         disabled={pending}
-        className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
+        className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-400 to-orange-400 group-hover:from-purple-400 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
       >
         <span
           className={clsx(
